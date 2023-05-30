c6dfca314fec9f57189914799b359e435b331233
berdiyar-desktop <berdiyare@gmail.com>
2023-05-31 00:26:19 +0500

    user2

--- sort.py
+++ sort.py
@@ -1,7 +1,7 @@
 def my_func(param):
     param.sort(reverse = True)
-    a = tuple(param)
-    return a
+    x = tuple(param)
+    return x
 i = list(map(int,input("Listni toldiring:").split()))
 res = my_func(i)
 print(res)
